96e7969b491cdc38904d25ebee08796e388aa3c5
Donald Chase Finlayson <donaldcfinlayson@gmail.com>
2016-11-27 15:36:15 -0800

    updating main.js

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,17 @@
 $(function() {
-
-  // your code will go here
-
+	$.ajax({
+    url: 'https://www.codeschool.com/users/3223798.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      for (var i = response.courses.completed.length - 1; i >= 0; i--) {
+      	course = response.courses.completed[i];
+      	$('#badges').append(
+      		'<div class = "course">'+
+      		'<h3>' + course.title + '</h3>' +
+      		'<img src="'+ course.badge +'">' +
+      		'<a target = "_blank" class="btn btn-primary" href="' + course.url + '"> See course </a>'
+      		+'</div>');      	
+      }
+    }
+  });
 });
