bd075d70651a1f825e14c4d1411ea7a0c8dc8c34
Donald Chase Finlayson <donaldcfinlayson@gmail.com>
2016-11-27 15:55:41 -0800

    updating main.js with more efficient code

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,15 +3,35 @@ $(function() {
     url: 'https://www.codeschool.com/users/3223798.json',
     dataType: 'jsonp',
     success: function(response) {
-      for (var i = response.courses.completed.length - 1; i >= 0; i--) {
-      	course = response.courses.completed[i];
-      	$('#badges').append(
-      		'<div class = "course">'+
-      		'<h3>' + course.title + '</h3>' +
-      		'<img src="'+ course.badge +'">' +
-      		'<a target = "_blank" class="btn btn-primary" href="' + course.url + '"> See course </a>'
-      		+'</div>');      	
-      }
+      	addCourses(response.courses.completed); 	
+      
     }
+
   });
+  function addCourses(courses){
+  	var $badges = $('#badges');
+  	courses.forEach(function(course){
+  		console.log(course);
+  		var $course = $('<div />', {
+  			'class': 'course'
+  		}).appendTo($badges);
+
+  		$('<h3/>', {
+  			'text' : course.title
+  		}).appendTo($course);
+
+  		$('<img/>', {
+  			'src' : course.badge
+  		}).appendTo($course);
+
+  		$('<a />', {
+  			'href' : course.url,
+  			'class' : 'btn btn-primary',
+  			text : 'See course',
+  			'target' : '_blank'
+  		}).appendTo($course);
+
+  	});
+  }
+
 });
